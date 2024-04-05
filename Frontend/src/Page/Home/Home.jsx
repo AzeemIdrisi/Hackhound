de2aad5d3b3fd960740a73ad1e88cdbc18bf726e
MridulTi <mridultiwari2002@gmail.com>
2024-04-06 04:17:35 +0530

    Fixed too many files f rupload

--- Frontend/src/Page/Home/Home.jsx
+++ Frontend/src/Page/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Barriers from '../../component/Barriers'
-import ParticleBg from '../../component/ParticleBg'
 import { Input } from '../../component/ui/input'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '../../component/ui/form'
 import { Button } from '../../component/ui/button'
@@ -22,21 +21,13 @@ export default function Home() {
   }
   return (
     <div className='w-screen h-screen grid place-items-center'>
-      <form onSubmit={handleSubmit} className="w-2/3 space-y-6">
+      <form onSubmit={handleSubmit} className="w-2/3 space-y-6 text-gray-10 ">
         <Input type="email" placeholder="Enter your Name"/>
         <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
         <Input type="file" required/>
         <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
         <Input type="file"/>
-        <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
-        <Input type="file"/>
-        <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
-        <Input type="file"/>
-        <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
-        <Input type="file"/>
-        <h1 className='font-bold text-lg'>Your 10th Marksheet</h1>
-        <Input type="file"/>
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="text-gray-5">Submit</Button>
       </form>
       </div>
   )
