a84675c02d4a8196292d0a43a9d664724e15467d
Devanshe sharma <devanshesharma6@gmail.com>
2024-04-05 16:00:04 +0530

    intial commit

--- Frontend/src/Page/Home/Home.jsx
+++ Frontend/src/Page/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Header from '../../component/Header'
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen grid place-items-center'>Home</div>
+    <div className='w-screen h-screen grid place-items-center'>
+      <Header/>
+      </div>
   )
 }
