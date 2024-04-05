e3907ffcf8e71b5a8ad057d9e51f6c7a6958e697
Devanshe sharma <devanshesharma6@gmail.com>
2024-04-05 19:04:23 +0530

    Added Css in Navbar

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -3,22 +3,26 @@ import "./style.css"
 
 export default function Navbar() {
   return (
-    <nav class="navbar">
-        <div class="navbar-left">
-            <div class="logo">
-                <img src="logo.png" alt="Logo"></img>
-            </div>
-            <ul class="nav-list">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <div class="navbar-right">
-            <button class="btn">Login</button>
-            <button class="btn">Sign Up</button>
-        </div>
-    </nav>
+    <><nav class="navbar">
+          <div class="navbar-left">
+              <div class="logo">
+                  <img src="logo.png" alt="Logo"></img>
+              </div>
+              <ul class="nav-list">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+          <div class="navbar-right">
+              <button class="btn">Login</button>
+              <button class="btn">Sign Up</button>
+          </div>
+      </nav><div class=''>
+              <h1 class=''>hello
+              </h1>
+          </div></>
+    
   )
 }
