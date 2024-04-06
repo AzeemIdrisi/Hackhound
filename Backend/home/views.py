3b42e648ae4127b1770e059136cd867f4b3699b6
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-04-06 10:46:52 +0530

    json

--- Backend/home/views.py
+++ Backend/home/views.py
@@ -2,24 +2,12 @@ from .forms import FileUploadForm
 from django.shortcuts import render
 from PIL import Image
 from home import ocr
+from django.http import JsonResponse
 
 
 # Create your views here.
 def home(request):
-    if request.method == "POST":
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Handle the file
-            file = form.cleaned_data["Marksheet"]
-            img = Image.open(file)
-            img_path = "home/uploads/marksheet.jpg"
-            img.save(img_path, "JPEG")
-            subject_marks = ocr.do_ocr(img_path)
-            # Do something with the file, like save it to the database or process it
-            return render(
-                request, "home/success.html", {"subject_marks": subject_marks}
-            )
-    else:
-        form = FileUploadForm()
-    # return render(request, "upload_file.html", {"form": form})
-    return render(request, "home/index.html", {"form": form})
+    img_path = "home/uploads/marksheet.jpg"
+    subject_marks = ocr.do_ocr(img_path)
+    # Do something with the file, like save it to the database or process it
+    return JsonResponse({"subject_marks": subject_marks})
